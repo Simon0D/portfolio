8c9aaa0e2697dc39e655ec6d5477064e94a07ceb
Simon0D <domokos.simon8@gmail.com>
2026-05-19 13:14:28 +0200

    Ok

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,21 @@
-import Image from "next/image";
+'use client';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-       <h1> fejlesztés alatt</h1>
+    <div className='modal show' style={{ display: 'block', position: 'initial' }}>
+      <Modal.Dialog>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal title</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>Modal body text goes here.</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant='secondary'>Close</Button>
+          <Button variant='primary'>Save changes</Button>
+        </Modal.Footer>
+      </Modal.Dialog>
     </div>
   );
 }
